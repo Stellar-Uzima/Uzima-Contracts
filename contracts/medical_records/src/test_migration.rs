c83f6b9677686fe07d00e72670abd47c4db4d1b2
JerryIdoko <onojajerome04@gmail.com>
2026-01-28 22:46:32 +0000

    chore: remove non-existent performance job and run fmt

--- contracts/medical_records/src/test_migration.rs
+++ contracts/medical_records/src/test_migration.rs
@@ -1,14 +1,14 @@
 #![cfg(test)] // Only compile this file when running tests
 
 // Allow unused imports temporarily while you build the test
-#[allow(unused_imports)] 
+#[allow(unused_imports)]
 use super::*;
 use soroban_sdk::Env;
 
 #[test]
 fn test_migration_initial_setup() {
     // Prefix with underscore (_) tells Rust: "I know this is unused right now"
-    let _env = Env::default(); 
-    
+    let _env = Env::default();
+
     // TODO: Add your actual migration test logic here
 }
