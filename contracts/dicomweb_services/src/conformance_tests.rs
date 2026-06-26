eff4add26a58ae8e8067bdc2ba717dfff6d5046d
nafiuishaaq <nafiuig@gmail.com>
2026-06-26 15:43:36 +0100

    implemented the onboarding

--- contracts/dicomweb_services/src/conformance_tests.rs
+++ contracts/dicomweb_services/src/conformance_tests.rs
@@ -31,12 +31,52 @@ fn test_wado_rs_retrieve_series_with_transfer_syntax() {
 
 // STOW-RS Store Tests
 #[test]
-fn test_stow_rs_store() {
-    // TODO: Implement STOW-RS store tests
+fn test_stow_rs_store_study() {
+    // TODO: Implement STOW-RS store study test
+}
+
+#[test]
+fn test_stow_rs_store_series() {
+    // TODO: Implement STOW-RS store series test
+}
+
+#[test]
+fn test_stow_rs_store_instance() {
+    // TODO: Implement STOW-RS store instance test
+}
+
+#[test]
+fn test_stow_rs_store_study_with_transfer_syntax() {
+    // TODO: Implement STOW-RS store study with transfer syntax test
+}
+
+#[test]
+fn test_stow_rs_store_series_with_transfer_syntax() {
+    // TODO: Implement STOW-RS store series with transfer syntax test
 }
 
 // QIDO-RS Query Tests
 #[test]
-fn test_qido_rs_query() {
-    // TODO: Implement QIDO-RS query tests
+fn test_qido_rs_query_studies() {
+    // TODO: Implement QIDO-RS query studies test
+}
+
+#[test]
+fn test_qido_rs_query_series() {
+    // TODO: Implement QIDO-RS query series test
+}
+
+#[test]
+fn test_qido_rs_query_instances() {
+    // TODO: Implement QIDO-RS query instances test
+}
+
+#[test]
+fn test_qido_rs_query_studies_with_modality() {
+    // TODO: Implement QIDO-RS query studies with modality test
+}
+
+#[test]
+fn test_qido_rs_query_series_with_modality() {
+    // TODO: Implement QIDO-RS query series with modality test
 }
